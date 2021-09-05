fixed vae visualization - vae seems to be working without kl divergence - currently latent space vars are entangled

vae/vae_visualizer.py
@@ -13,22 +13,21 @@ x_test = x_test / 255.
 encoder = tf.keras.models.load_model("encoder.h5")
 decoder = tf.keras.models.load_model("decoder.h5")
 
-for i in range(0, len(x_test)):
-    image = x_test[i]
-    # print("Original Image Shape", np.shape(image))
-    cv2.imshow("Original Image", cv2.resize(image, (360, 360)))
-    encoded_image = encoder.predict(np.expand_dims(image, 0))
+mean_vector, std_vector = encoder.predict(x_test)
 
-    # print("Image squeezed to encoded shape: ", np.shape(encoded_image))
-    # print(encoded_image)
-    generated_image = cv2.resize(decoder.predict(encoded_image)[0,:,:,:], (360, 360))
-    # print(np.shape(generated_image))
-    cv2.imshow("Generated Image", generated_image)
+# Sampling from gaussian latent space
+z_vector = mean_vector + tf.multiply(std_vector, tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
+
+generated_images = decoder.predict(z_vector)
+
+for i in range(0, len(generated_images)):
+    cv2.imshow("Original", cv2.resize(x_test[i], (360, 360)))
+    cv2.waitKey(1)
+
+    cv2.imshow("Generated", cv2.resize(generated_images[i], (360, 360)))
     cv2.waitKey(1)
 
     if i != 0:
         input() # wait till enter pressed to load next image
-
-    # time.sleep(3)
 
 
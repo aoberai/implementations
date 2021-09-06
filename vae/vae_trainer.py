2aeef92543974e1e4a0bb30f9ee815c948ef548f
aoberai <aoberai05@gmail.com>
2021-09-06 00:28:34 -0700

    changed to log var vector, still not working

--- vae/vae_trainer.py
+++ vae/vae_trainer.py
@@ -9,6 +9,7 @@ import cv2
 import numpy as np
 import tensorflow.keras.backend as K
 import time
+import math
 import sys
 
 tf.config.run_functions_eagerly(True)
@@ -44,9 +45,9 @@ def encoder(input_shape, output_latent_space_dims):
 
   # Bottleneck
   mean_vector = layers.Dense(output_latent_space_dims)(x)
-  std_vector = layers.Dense(output_latent_space_dims)(x)
+  log_var_vector = layers.Dense(output_latent_space_dims)(x)
 
-  encoder = Model(inputs, (mean_vector, std_vector), name="Encoder")
+  encoder = Model(inputs, (mean_vector, log_var_vector), name="Encoder")
   print(encoder.summary())
   return encoder
 
@@ -79,22 +80,37 @@ def reconstruction_loss(y_true, y_pred):
     return loss
 
 # Kullback–Leibler divergence
-def kl_divergence_loss(mean_vector, std_vector):
-    return -0.5 * K.sum(1 + K.log(1e-8 + K.square(std_vector)) - K.square(mean_vector) - K.square(std_vector), axis=1)
+# TODO: Read this: https://stats.stackexchange.com/questions/486203/why-we-learn-log-sigma2-in-vae-reparameterization-trick-instead-of-standar
+def kl_divergence_loss(mean_vector, log_var_vector):
+    # Calculates divergence to N(mu = 0, sigma**2 = 1)
+    loss = -0.5 * K.sum(1 + log_var_vector - K.square(mean_vector) - K.exp(log_var_vector), axis=1)
+    return loss
+    # return -0.5 * K.sum(K.square(std_vector) + K.square(mean_vector) - 1 - K.log(1e-8 + K.square(std_vector)), axis=1)
 
 # This annotation causes the function to be "compiled".
 @tf.function
 def train_step(images):
     with tf.GradientTape() as encoder, tf.GradientTape() as decoder:
-      mean_vector, std_vector = encoder_model(images, training=True)
+      mean_vector, log_var_vector = encoder_model(images, training=True)
+
+      if math.isnan(np.mean(mean_vector.numpy())):
+          print("\n\n\n\n")
+          print(mean_vector.numpy())
+          print(log_var_vector.numpy())
+          exit(0)
+      else:
+          print("Works")
+
 
       # Sampling from gaussian latent space
-      z_vector = mean_vector + tf.multiply(std_vector, tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
+      z_vector = mean_vector + tf.multiply(tf.exp(0.5 * log_var_vector), tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
 
       generated_images = decoder_model(z_vector, training=True)
 
-      loss = reconstruction_loss(images, generated_images) # reconstruction: calculates MSE difference between original and generated image
-      # loss += kl_divergence_loss(mean_vector, std_vector) # regularization: calculates kullback leibler divergence between normal distribution and distribution for each latent space variable
+      loss = 0
+      loss += reconstruction_loss(images, generated_images) # reconstruction: calculates MSE difference between original and generated image
+      loss += kl_divergence_loss(mean_vector, log_var_vector) # regularization: calculates kullback leibler divergence between normal distribution and distribution for each latent space variable
+
 
     # Computes gradient through backpropping through operations specified in gradient tape section
     encoder_gradients = encoder.gradient(loss, encoder_model.trainable_variables)
@@ -110,14 +126,14 @@ def train(dataset, epochs=10):
     validation_loss = []
     training_loss = []
 
-    mean_vector, std_vector = encoder_model.predict(x_test)
+    mean_vector, log_var_vector = encoder_model.predict(x_test)
 
     # Sampling from gaussian latent space
-    z_vector = mean_vector + tf.multiply(std_vector, tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
+    z_vector = mean_vector + tf.multiply(0.5 * tf.exp(log_var_vector), tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
 
     generated_images = decoder_model.predict(z_vector)
 
-    print("Epoch 0 Validation Loss:", np.mean(reconstruction_loss(x_test, generated_images))) # reconstruction: calculates MSE difference between original and generated image
+    print("Epoch 0 Validation Loss:", np.mean(reconstruction_loss(x_test, generated_images) + kl_divergence_loss(mean_vector, log_var_vector)))
 
     try:
         for epoch in range(epochs):
@@ -130,17 +146,14 @@ def train(dataset, epochs=10):
               counter+=1
               print('Epoch Completed: {0:3f}'.format(counter / dataset.__len__().numpy()), end='\r')
 
-            # validation_loss.append(np.square(x_test - decoder_model.predict(encoder_model.predict(x_test))).mean())
-
-            mean_vector, std_vector = encoder_model.predict(x_test)
+            mean_vector, log_var_vector = encoder_model.predict(x_test)
 
             # Sampling from gaussian latent space
-            z_vector = mean_vector + tf.multiply(std_vector, tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
+            z_vector = mean_vector + tf.multiply(0.5 * tf.exp(log_var_vector), tf.random.normal(tf.shape(mean_vector), 0, 1, tf.float32))
 
             generated_images = decoder_model.predict(z_vector)
 
-            validation_loss.append(np.mean(reconstruction_loss(x_test, generated_images))) # reconstruction: calculates MSE difference between original and generated image
-            # loss += kl_divergence_loss(mean_vector, std_vector) # regularization: calculates kullback leibler divergence between normal distribution and distribution for each latent space variable
+            validation_loss.append(np.mean(reconstruction_loss(x_test, generated_images) + kl_divergence_loss(mean_vector, log_var_vector)))
 
             training_loss.append(np.mean(np.array(loss_average)))
             print('\nTime for epoch {} is {} sec; Training loss : {} Validation Loss: {} Counter : {}'.format(epoch + 1, time.time()-start_time, training_loss[-1], validation_loss[-1], counter))

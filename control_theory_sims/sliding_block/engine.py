ba12747789bb3edd3c05abf847043c1573bdeff2
aoberai <aoberai05@gmail.com>
2021-10-08 18:11:03 -0700

    real world units

--- control_theory_sims/sliding_block/engine.py
+++ control_theory_sims/sliding_block/engine.py
@@ -1,27 +1,27 @@
 import time
 import pygame
+import constants as ct
 
 class _Block:
     g = 9.8
     def __init__(self, mass, x_init, v_init, s_coef_fric, k_coef_fric, display_dims):
         self.mass = mass
-        self.x_init = x_init[0]
-        self.v_init = v_init[0]
+        self.x_init = x_init
+        self.v_init = v_init
         self.s_coef_fric = s_coef_fric
         self.k_coef_fric = k_coef_fric
-        self.x_h = self.x_init
-        self.v_h = self.v_init 
+        self.x_h = self.x_init[0]
+        self.v_h = self.v_init[0]
         self.init_time = time.time()
         self.display_dims = display_dims
-        pass
     def step(self, current_time, applied_f):
         normal_f = self.g*self.mass
         fric_f = normal_f * self.k_coef_fric
         net_f = (applied_f - fric_f)
         a = net_f/self.mass
         d_time = current_time - self.init_time
-        self.x_h = self.x_init + self.v_init*d_time + 0.5*a*d_time**2
-        self.v_h = self.v_init + a*d_time
+        self.x_h = self.x_init[0] + self.v_init[0]*d_time + 0.5*a*d_time**2
+        self.v_h = self.v_init[0] + a*d_time
         return (self.x_h, self.v_h, net_f)
 
 class _Surface:
@@ -33,25 +33,28 @@ class _Surface:
         pass
 
 class Components:
-    def __init__(self, mass, surface_width, surface_height, x_init, v_init, s_coef_fric, k_coef_fric, block_display_dims, wants_render=True):
+    def __init__(self, mass, surface_width, surface_height, x_init, v_init, s_coef_fric, k_coef_fric, block_display_dims, wants_render=True, screen_dims=(5, 5)):
+        '''
+        All units, including display_dims, is in meters
+        '''
         self._block = _Block(mass, x_init, v_init, s_coef_fric, k_coef_fric, display_dims=block_display_dims)
         self._surface = _Surface(surface_width, surface_height)
+        self.screen_dims = screen_dims
         if wants_render:
-            self._GREY   = (210, 210 ,210)
-            self._WHITE  = (255, 255, 255)
-            self._SCREEN_WIDTH = 1000
-            self._SCREEN_HEIGHT = 700
             pygame.init()
-            self.pyg_surface = pygame.display.set_mode((self._SCREEN_WIDTH, self._SCREEN_HEIGHT,))
-            
+            self.pyg_surface = pygame.display.set_mode((int(screen_dims[0]*ct.PIXELS_PER_METER), int(screen_dims[1]*ct.PIXELS_PER_METER)))
     def step(self, current_time, applied_f):
         self._surface.step()
+        # TODO: add contact param which checks for contact from all sides
         block_state = self._block.step(current_time, applied_f)
         return block_state
     def render(self, block_state):
         self.pyg_surface.fill((0, 0, 0))
-        pygame.draw.rect(self.pyg_surface, self._GREY, pygame.Rect(0, self._SCREEN_HEIGHT - self._SCREEN_HEIGHT * self._surface.height, self._surface.width * self._SCREEN_WIDTH, self._surface.height * self._SCREEN_HEIGHT))
-        pygame.draw.rect(self.pyg_surface, self._WHITE, pygame.Rect((block_state[0], self._SCREEN_HEIGHT - self._SCREEN_HEIGHT * self._surface.height - self._block.display_dims[1]), (self._block.display_dims[0], self._block.display_dims[1])))
+        # TODO: put pygame rect in class
+        surf_rect = pygame.Rect(0, int(ct.PIXELS_PER_METER*(self.screen_dims[1]-self._surface.height)), int(self._surface.width*self.screen_dims[0]*ct.PIXELS_PER_METER**2), int(self._surface.height*self.screen_dims[1]*ct.PIXELS_PER_METER**2))
+        block_rect = pygame.Rect(int(ct.PIXELS_PER_METER*block_state[0]), int(ct.PIXELS_PER_METER*(self.screen_dims[1] - self._surface.height - self._block.display_dims[1])), int(ct.PIXELS_PER_METER*self._block.display_dims[0]), int(ct.PIXELS_PER_METER*self._block.display_dims[1]))
+        pygame.draw.rect(self.pyg_surface, ct.GREY, surf_rect)
+        pygame.draw.rect(self.pyg_surface, ct.WHITE, block_rect)
         pygame.display.flip()
 
 
--- control_theory_sims/sliding_block/runner.py
+++ control_theory_sims/sliding_block/runner.py
@@ -1,12 +1,16 @@
 from engine import Components
 import time
+import math
 
 # Implement y component forces and interactions with static obj
-components = Components(mass=10, surface_width=1, surface_height=0.1, x_init=(2, 0), v_init=(10, 0), s_coef_fric=0.3, k_coef_fric=0.15, block_display_dims=(50, 50), wants_render=True)
+components = Components(mass=10, surface_width=0.3, surface_height=0.01, x_init=(0.1, 0), v_init=(0.01, 0), s_coef_fric=0.3, k_coef_fric=0.15, block_display_dims=(0.01, 0.01), wants_render=True, screen_dims=(0.2, 0.2))
+
+def force_func(time):
+    return 5*(math.sin(time)+0.5)
 
 while True:
     curr_time = time.time()
-    x_h, v_h, f_h = components.step(curr_time, applied_f=0.1)
+    x_h, v_h, f_h = components.step(curr_time, applied_f=force_func(curr_time))
     components.render(block_state=(x_h, v_h,))
     print("Position: %0.3f; Velocity: %0.3f, Net Horizontal Force: %0.3f" % (x_h, v_h, f_h))
     time.sleep(0.1)

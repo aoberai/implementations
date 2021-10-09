4a54c3c80c170f1fc2da102bd17a2876ad38b6e8
aoberai <aoberai05@gmail.com>
2021-10-09 16:55:15 -0700

    sliding block friction fixed

--- control_theory_sims/sliding_block/runner.py
+++ control_theory_sims/sliding_block/runner.py
@@ -1,18 +1,20 @@
-from engine import Components
+from simulation import Simulation
 import time
-import math
 
 # Implement y component forces and interactions with static obj
-components = Components(mass=10, surface_width=0.3, surface_height=0.01, x_init=(0.1, 0), v_init=(0.01, 0), s_coef_fric=0.3, k_coef_fric=0.15, block_display_dims=(0.01, 0.01), wants_render=True, screen_dims=(0.2, 0.2))
+simulation = Simulation(mass=1, surface_width=0.5, surface_height=0.01, x_init=(0, 0), v_init=(0.3, 0), s_coef_fric=0.03, k_coef_fric=0.015, block_display_dims=(0.01, 0.01), wants_render=True, screen_dims=(0.5, 0.2))
 
-def force_func(time):
-    return 5*(math.sin(time)+0.5)
+reference_x = (0.1, 0)
+
+applied_f = 0
+x_h, v_h, f_h = 0, 0, 0
 
 while True:
     curr_time = time.time()
-    x_h, v_h, f_h = components.step(curr_time, applied_f=force_func(curr_time))
-    components.render(block_state=(x_h, v_h,))
-    print("Position: %0.3f; Velocity: %0.3f, Net Horizontal Force: %0.3f" % (x_h, v_h, f_h))
-    time.sleep(0.1)
+    x_h, v_h, f_h = simulation.step(curr_time, applied_f=applied_f)
+    error = (reference_x[0] - x_h)
+    simulation.render(block_state=(x_h, v_h,), reference_x=reference_x)
+    print("Error: %0.5f; Applied Force %0.5f; Position: %0.3f; Velocity: %0.3f, Net Horizontal Force: %0.3f" % (error, applied_f, x_h, v_h, f_h))
+    # input()
 
 
--- control_theory_sims/sliding_block/simulation.py
+++ control_theory_sims/sliding_block/simulation.py
@@ -12,16 +12,17 @@ class _Block:
         self.k_coef_fric = k_coef_fric
         self.x_h = self.x_init[0]
         self.v_h = self.v_init[0]
-        self.init_time = time.time()
+        self.last_time = time.time()
         self.display_dims = display_dims
     def step(self, current_time, applied_f):
         normal_f = self.g*self.mass
-        fric_f = normal_f * self.k_coef_fric
-        net_f = (applied_f - fric_f)
+        fric_f, self.v_h = (-(normal_f * self.k_coef_fric), self.v_h) if abs(self.v_h) > 0.02 else (min(abs(applied_f), abs(self.s_coef_fric * normal_f)), 0)
+        net_f = (applied_f + fric_f)
         a = net_f/self.mass
-        d_time = current_time - self.init_time
-        self.x_h = self.x_init[0] + self.v_init[0]*d_time + 0.5*a*d_time**2
-        self.v_h = self.v_init[0] + a*d_time
+        d_time = current_time - self.last_time
+        self.x_h = self.x_h + self.v_h*d_time + 0.5*a*d_time**2
+        self.v_h += a*d_time
+        self.last_time = time.time()
         return (self.x_h, self.v_h, net_f)
 
 class _Surface:
@@ -32,7 +33,7 @@ class _Surface:
     def step(self):
         pass
 
-class Components:
+class Simulation:
     def __init__(self, mass, surface_width, surface_height, x_init, v_init, s_coef_fric, k_coef_fric, block_display_dims, wants_render=True, screen_dims=(5, 5)):
         '''
         All units, including display_dims, is in meters
@@ -40,6 +41,7 @@ class Components:
         self._block = _Block(mass, x_init, v_init, s_coef_fric, k_coef_fric, display_dims=block_display_dims)
         self._surface = _Surface(surface_width, surface_height)
         self.screen_dims = screen_dims
+        self.reference_rect_dims = (0.002, 0.01)
         if wants_render:
             pygame.init()
             self.pyg_surface = pygame.display.set_mode((int(screen_dims[0]*ct.PIXELS_PER_METER), int(screen_dims[1]*ct.PIXELS_PER_METER)))
@@ -48,13 +50,16 @@ class Components:
         # TODO: add contact param which checks for contact from all sides
         block_state = self._block.step(current_time, applied_f)
         return block_state
-    def render(self, block_state):
+    def render(self, block_state, reference_x):
         self.pyg_surface.fill((0, 0, 0))
         # TODO: put pygame rect in class
         surf_rect = pygame.Rect(0, int(ct.PIXELS_PER_METER*(self.screen_dims[1]-self._surface.height)), int(self._surface.width*self.screen_dims[0]*ct.PIXELS_PER_METER**2), int(self._surface.height*self.screen_dims[1]*ct.PIXELS_PER_METER**2))
         block_rect = pygame.Rect(int(ct.PIXELS_PER_METER*block_state[0]), int(ct.PIXELS_PER_METER*(self.screen_dims[1] - self._surface.height - self._block.display_dims[1])), int(ct.PIXELS_PER_METER*self._block.display_dims[0]), int(ct.PIXELS_PER_METER*self._block.display_dims[1]))
+        reference_rect = pygame.Rect(int(ct.PIXELS_PER_METER*reference_x[0]), int(ct.PIXELS_PER_METER*(self.screen_dims[1] - self._surface.height - self.reference_rect_dims[1])), int(ct.PIXELS_PER_METER*self.reference_rect_dims[0]), int(ct.PIXELS_PER_METER*self.reference_rect_dims[1]))
+        # print(int(ct.PIXELS_PER_METER*reference_x[0]), int(ct.PIXELS_PER_METER*(self.screen_dims[1] - self._surface.height + self.reference_rect_dims[1])), int(ct.PIXELS_PER_METER*self.reference_rect_dims[0]), int(ct.PIXELS_PER_METER*self.reference_rect_dims[1]))
         pygame.draw.rect(self.pyg_surface, ct.GREY, surf_rect)
         pygame.draw.rect(self.pyg_surface, ct.WHITE, block_rect)
+        pygame.draw.rect(self.pyg_surface, ct.RED, reference_rect)
         pygame.display.flip()
 
 

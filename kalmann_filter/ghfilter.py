f155011bf9b1d287205465fd2febb00ea53dd356
aoberai <aoberai05@gmail.com>
2021-10-30 16:41:09 -0700

    some comments

--- kalmann_filter/ghfilter.py
+++ kalmann_filter/ghfilter.py
@@ -31,7 +31,7 @@ class GHFilter:
     Performs g-h filter on 1 state variable with a fixed g and h.
     'data' contains the data to be filtered.
     'x0' is the initial value for our state variable
-    'dx' is the change rate for our state variable
+    'v' is the change rate for our state variable
     'g' is the g-h's g scale factor 'h' is the g-h's h scale factor
     'dt' is the length of the time step
 
@@ -41,14 +41,14 @@ class GHFilter:
 
     'p' denotes plus; 'm' denotes minus
     """
-    def __init__(self, x0, dx, g, h, dt):
+    def __init__(self, x0, v, g, h, dt):
         self.x0 = self.x = x0
-        self.dx = dx
+        self.v = v
         self.g = g
         self.h = h
         self.dt = dt
     def predict(self):
-        self.x_pred = self.dx * self.dt + self.x
+        self.x_pred = self.v * self.dt + self.x
         # TODO: predict both future pos and future gain rate
         return self.x_pred
     def update(self, z):
@@ -56,12 +56,12 @@ class GHFilter:
         # updates x estimation
         self.x = self.x_pred + self.g*resid
         # updates change in model for next timestep prediction 
-        self.dx += self.h*resid/self.dt
+        self.v += self.h*resid/self.dt
         return self.x
 
 class Utils:
-    def get_data(self, n, x0, dx, dt, mu, sigma):
-        return [x0 + dx*dt*i + sigma * np.random.randn() + mu for i in range(n)]
+    def get_data(self, n, x0, v, dt, mu, sigma, a=0):
+        return [x0 + 0.5 * a * (i * dt) ** 2 + v*dt*i + sigma * np.random.randn() + mu for i in range(n)]
     def plot(self, y_set, dt):
         series = []
         for y in range(len(y_set.values())):
@@ -71,10 +71,23 @@ class Utils:
         plt.show()
 
 if __name__ == "__main__":
-    x0_obs, x0_guess, dx, g, h, dt = 5, 100, 2, 0.2, 0.02, 1
+    '''
+     Larger g -> follow measurement more than prediction
+     Smaller g -> follow prediction more than measurement
+     Optimize g such that can tell change in measurement due to unexpected force (not in model) while reducing noise through following more stable prediction
+
+     Larger h -> follow measurement of v more than prediction
+     Smaller g -> follow the measurement of v less than prediction
+
+     If signal changing rapidly, want smaller g (relies on measurement more than prediction)
+     If signal changing slowly, want larger g (relies on prediction more than measurement)
+    '''
+    x0_obs, x0_guess, v, g, h, dt = 5, 100, 2, 0.2, 0.02, 1
     utils = Utils()
-    ghfilter = GHFilter(x0_guess, dx, g, h, dt)
-    observed_data = utils.get_data(n=100, x0=x0_obs, dx=dx, dt=1, mu=0, sigma=5)
+    ghfilter = GHFilter(x0_guess, v, g, h, dt)
+    # Acceleration show that gh filter can only model constant speed data
+    # Need third term k to weight acceleration estimate
+    observed_data = utils.get_data(n=10, x0=x0_obs, v=v, dt=1, mu=0, sigma=5, a=10)
     xs_estimated = []
     predicted = []
     for z in observed_data:

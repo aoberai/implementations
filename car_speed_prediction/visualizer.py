39cd7857a8f61025d1df895ae2309fa49bd665a7
aoberai <aoberai05@gmail.com>
2021-11-13 11:51:43 -0800

    hidden stuffs

--- car_speed_prediction/visualizer.py
+++ car_speed_prediction/visualizer.py
@@ -9,19 +9,20 @@ op_flow = None
 
 def preprocess(frame):
     global op_flow
-    frame = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)[260:260+90, 180:180+260] , ct.IMG_SIZE[0:2])
-    # roi = np.transpose(cv2.resize(cv2.cvtColor(cv2.imread("roi_mask.jpg"), cv2.COLOR_BGR2GRAY), np.shape(frame)[0:2]))
-    # print(np.shape(frame), np.shape(roi))
-    # frame = cv2.bitwise_and(frame, roi)
+    frame = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)[260:260+90, 180:180+260], ct.IMG_SIZE[0:2])
     if op_flow is None:
         op_flow = DenseOpFlow(cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)) # TODO: this scuffed
     opflow_frame = cv2.cvtColor(op_flow.get(frame), cv2.COLOR_BGR2GRAY)
     '''
     Visualization
     '''
-    # cv2.imshow("Frame", cv2.resize(np.hstack([frame, opflow_frame]), tuple(
-    #     [scale_factor := 7 * i for i in ct.IMG_SIZE[0:2]])))
-    # cv2.waitKey(1)
+    cv2.imshow("Frame", cv2.resize(frame, tuple(
+        [scale_factor := 7 * i for i in ct.IMG_SIZE[0:2]])))
+    cv2.imshow(
+        "Speed Predictor",
+        cv2.resize(cv2.cvtColor(opflow_frame.astype('float32'), cv2.COLOR_GRAY2BGR), (ct.IMG_SIZE[0] * 3, ct.IMG_SIZE[1] * 3)))
+
+    cv2.waitKey(1)
 
     frame = frame / 255
     opflow_frame = opflow_frame / 255
@@ -65,7 +66,6 @@ if __name__ == "__main__":
     position = round(args.pos * vid.get(cv2.CAP_PROP_FRAME_COUNT))
     vid.set(cv2.CAP_PROP_POS_FRAMES, position)
 
-    # op_flow = DenseOpFlow(cv2.resize(vid.read()[1], ct.IMG_SIZE[0:2]))
     speed_predictor = tf.keras.models.load_model(args.model)
     model_frames = []
     counter = position
@@ -82,12 +82,7 @@ if __name__ == "__main__":
             predicted_speed = speed_predictor.predict(frames_np)
             cv2.imshow(
                 "Speed Predictor",
-                cv2.resize(
-                    frame,
-                    (ct.IMG_SIZE[0] *
-                     3,
-                     ct.IMG_SIZE[1] *
-                     3)))
+                cv2.resize(cv2.cvtColor(model_frames[-1].astype('float32') * 255, cv2.COLOR_GRAY2BGR), (ct.IMG_SIZE[0] * 3, ct.IMG_SIZE[1] * 3)))
             cv2.waitKey(1)
             print("Predicted Speed:",
                   predicted_speed[0][0],
--- car_speed_prediction/visualizer_v2.py
+++ car_speed_prediction/visualizer_v2.py
@@ -3,12 +3,15 @@ import cv2
 import sys
 import constants as ct
 from dflow import DenseOpFlow
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 np.set_printoptions(threshold=sys.maxsize)
 
 train_vid = cv2.VideoCapture("./data/train.mp4")
 test_vid = cv2.VideoCapture("data/test.mp4")
 train_speed = open("data/train.txt")
+speeds = train_speed.read().split()
 op_flow = None
 
 train_X_img = []
@@ -20,8 +23,8 @@ test_X = []
 trainset_size = train_vid.get(cv2.CAP_PROP_FRAME_COUNT)
 testset_size = test_vid.get(cv2.CAP_PROP_FRAME_COUNT)
 
-start_frame = 0*train_vid.get(cv2.CAP_PROP_FRAME_COUNT)
-train_vid.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+frame_index = int(0.6*train_vid.get(cv2.CAP_PROP_FRAME_COUNT))
+train_vid.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
 
 def preprocess(frame):
     global op_flow
@@ -38,26 +41,43 @@ def preprocess(frame):
     cv2.imshow("Frame", cv2.resize(np.hstack([frame, opflow_frame]), tuple(
         [scale_factor := 7 * i for i in ct.IMG_SIZE[0:2]])))
     cv2.waitKey(1)
+    print(max(opflow_frame[0])/255.0)
 
-    frame = frame / 255
-    opflow_frame = opflow_frame / 255
+    frame = frame / 255.0
+    opflow_frame = opflow_frame / 255.0
     return (frame, opflow_frame)
 
+speed_predictor = tf.keras.models.load_model(ct.MODEL_PATH)
+model_frames = []
+predicted_speeds = []
 
-counter = 0
 while True:
     ret, frame = train_vid.read()
     if not ret:
         break
     data = preprocess(frame)
-    speed = float(train_speed.readline())
     train_X_img.append(data[0])
     train_X_flow.append(data[1])
-    train_Y.append(speed)
-    print("Speed:", speed)
-    counter += 1
-    print(counter / trainset_size)
+    speed = speeds[frame_index]
 
-np.save("frames", np.array(train_X_flow))
-np.save("flow", np.array(train_X_flow))
-np.save("speeds", np.array(train_Y))
+    model_frames.append(data[1])
+
+    if len(model_frames) > ct.TIMESTEPS:
+        del model_frames[0]
+        print(np.array(model_frames).dtype)
+        frames_np = np.expand_dims(np.array(model_frames).astype("float32"), 0)
+        predicted_speeds.append(predicted_speed := speed_predictor.predict(frames_np))
+        # cv2.imshow("Flow", cv2.cvtColor(train_X_flow[-1].astype("float32"), cv2.COLOR_GRAY2BGR))
+        # cv2.waitKey(1)
+        print("Predicted Speed:",
+              predicted_speed[0][0],
+              "Real Speed:", speed)
+    frame_index+=1
+
+series = []
+series.append(plt.scatter(
+    np.array(predicted_speeds), np.array([i for i in range(len(predicted_speeds))]), 5))
+plt.legend(series, ["prediction"], fontsize=8, loc='upper left')
+plt.show()
+
+

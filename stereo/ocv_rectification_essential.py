db3eef5f3383b8c9628b5b333ddc37a0dc04aa9e
aoberai <aoberai05@gmail.com>
2022-01-04 01:27:40 -0800

    fixed stereo by using certain matrices from calibration and not stereorectify method

--- stereo/ocv_rectification_essential.py
+++ stereo/ocv_rectification_essential.py
@@ -1,29 +1,33 @@
 import cv2
 import numpy as np
 
-imgL = cv2.imread("l_cap2.png",0)
-imgR = cv2.imread("r_cap2.png",0)
+imgL = cv2.imread("l_cap.png",0)
+imgR = cv2.imread("r_cap.png",0)
 # TODO: this might be breaking things
 # imgL = cv2.resize(imgL, size)
 # imgR = cv2.resize(imgR, size)
 size = (np.shape(imgL)[1], np.shape(imgL)[0])
 
-left_cam_file = cv2.FileStorage("left_cam.yml", cv2.FILE_STORAGE_READ)
-right_cam_file = cv2.FileStorage("right_cam.yml", cv2.FILE_STORAGE_READ)
-stereo_file = cv2.FileStorage("stereo_cam.yml", cv2.FILE_STORAGE_READ)
+# left_cam_file = cv2.FileStorage("left_cam.yml", cv2.FILE_STORAGE_READ)
+# right_cam_file = cv2.FileStorage("right_cam.yml", cv2.FILE_STORAGE_READ)
+cv_file = cv2.FileStorage("stereo_cam.yml", cv2.FILE_STORAGE_READ)
 
-K1 = left_cam_file.getNode("K").mat()
-D1 = left_cam_file.getNode("D").mat()
+# TODO: I fixed by not using stereo rectify and using these matrices from calibration itself
+K1 = cv_file.getNode("K1").mat()
+D1 = cv_file.getNode("D1").mat()
+K2 = cv_file.getNode("K2").mat()
+D2 = cv_file.getNode("D2").mat()
+R = cv_file.getNode("R").mat()
+T = cv_file.getNode("T").mat()
+E = cv_file.getNode("E").mat()
+F = cv_file.getNode("F").mat()
+R1 = cv_file.getNode("R1").mat()
+R2 = cv_file.getNode("R2").mat()
+P1 = cv_file.getNode("P1").mat()
+P2 = cv_file.getNode("P2").mat()
+Q = cv_file.getNode("Q").mat()
 
-K2 = right_cam_file.getNode("K").mat()
-D2 = right_cam_file.getNode("D").mat()
-
-R = stereo_file.getNode("R").mat()
-# R = np.eye(3, 3)
-# print(R)
-T = stereo_file.getNode("T").mat()
-
-R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(cameraMatrix1=K1, distCoeffs1=D1, cameraMatrix2=K2, distCoeffs2=D2, imageSize=size, R=R, T=T, flags=cv2.CALIB_ZERO_DISPARITY, alpha=-1)
+# R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(cameraMatrix1=K1, distCoeffs1=D1, cameraMatrix2=K2, distCoeffs2=D2, imageSize=size, R=R, T=T, flags=cv2.CALIB_ZERO_DISPARITY, alpha=-1)
 
 leftMapX, leftMapY = cv2.initUndistortRectifyMap(K1, D1, R1, P1, size, cv2.CV_32FC1)
 left_rectified = cv2.remap(imgL, leftMapX, leftMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
---
+++ stereo/stereo_img.py
@@ -0,0 +1,96 @@
+import numpy as np
+import cv2
+import argparse
+import sys
+
+def depth_map(imgL, imgR):
+    """ Depth map calculation. Works with SGBM and WLS. Need rectified images, returns depth map ( left to right disparity ) """
+    # SGBM Parameters -----------------
+    window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+
+    left_matcher = cv2.StereoSGBM_create(
+        minDisparity=-1,
+        numDisparities=5*16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+        blockSize=window_size,
+        P1=8 * 3 * window_size,
+        # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+        P2=32 * 3 * window_size,
+        disp12MaxDiff=12,
+        uniquenessRatio=10,
+        speckleWindowSize=50,
+        speckleRange=32,
+        preFilterCap=63, mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY)
+    right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+    # FILTER Parameters
+    lmbda = 80000
+    sigma = 1.3
+    visual_multiplier = 6
+
+    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+    wls_filter.setLambda(lmbda)
+
+    wls_filter.setSigmaColor(sigma)
+    displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+    dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+    displ = np.int16(displ)
+    dispr = np.int16(dispr)
+    filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
+
+    filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
+    filteredImg = np.uint8(filteredImg)
+
+    return filteredImg
+
+if __name__ == '__main__':
+    # FILE_STORAGE_READ
+    cv_file = cv2.FileStorage("stereo_cam.yml", cv2.FILE_STORAGE_READ)
+
+    # note we also have to specify the type to retrieve other wise we only get a
+    # FileNode object back instead of a matrix
+    K1 = cv_file.getNode("K1").mat()
+    D1 = cv_file.getNode("D1").mat()
+    K2 = cv_file.getNode("K2").mat()
+    D2 = cv_file.getNode("D2").mat()
+    R = cv_file.getNode("R").mat()
+    T = cv_file.getNode("T").mat()
+    E = cv_file.getNode("E").mat()
+    F = cv_file.getNode("F").mat()
+    R1 = cv_file.getNode("R1").mat()
+    R2 = cv_file.getNode("R2").mat()
+    P1 = cv_file.getNode("P1").mat()
+    P2 = cv_file.getNode("P2").mat()
+    Q = cv_file.getNode("Q").mat()
+    while True:  # Loop until 'q' pressed or stream ends
+        # Grab&retreive for sync images
+        leftFrame =  cv2.imread("l_cap.png")
+        rightFrame =  cv2.imread("r_cap.png")
+        height, width, channel = leftFrame.shape  # We will use the shape for remap
+
+        # Undistortion and Rectification part!
+        leftMapX, leftMapY = cv2.initUndistortRectifyMap(K1, D1, R1, P1, (width, height), cv2.CV_32FC1)
+        left_rectified = cv2.remap(leftFrame, leftMapX, leftMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+        rightMapX, rightMapY = cv2.initUndistortRectifyMap(K2, D2, R2, P2, (width, height), cv2.CV_32FC1)
+        right_rectified = cv2.remap(rightFrame, rightMapX, rightMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+
+        cv2.imshow("win", cv2.hconcat([left_rectified, right_rectified]))
+        cv2.waitKey(0)
+        exit(0)
+
+        # We need grayscale for disparity map.
+        gray_left = cv2.cvtColor(left_rectified, cv2.COLOR_BGR2GRAY)
+        gray_right = cv2.cvtColor(right_rectified, cv2.COLOR_BGR2GRAY)
+
+        disparity_image = depth_map(gray_left, gray_right)  # Get the disparity map
+
+        # Show the images
+        cv2.imshow('left(R)', leftFrame)
+        cv2.imshow('right(R)', rightFrame)
+        cv2.imshow('Disparity', disparity_image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Get key to stop stream. Press q for exit
+            break
+
+    # Release the sources.
+    cap_left.release()
+    cap_right.release()
+    cv2.destroyAllWindows()

f20086aa7c55065eef02bbdbeeadef550caf3450
aoberai <aoberai05@gmail.com>
2023-07-08 05:55:06 -0700

    works now -- adding end of game 0 value and removing intermediate np ops for action selection which blocked optimization fixed it

---
+++ dqn/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class DQN(nn.Module):
+
+    def __init__(self, obs_dim, action_dim):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(obs_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.head = nn.Linear(128, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.head(x)
+        return x
+
+
---
+++ reinforce/viz.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+import torch
+from model import DQN
+
+device = torch.device('cuda')
+policy_model = torch.load("reinforce.pt", map_location=device)
+env = gym.make("CartPole-v1", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    # agent policy that uses the observation and info
+    # action = env.action_space.sample()
+    action = torch.argmax(policy_model(torch.as_tensor(observation, device=device).unsqueeze(0))).item()
+    print(_, action)
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()

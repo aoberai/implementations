4b24a2fe6d8202c094132354ea2b758a8a9b6bad
aoberai <aoberai05@gmail.com>
2023-07-16 02:03:15 -0700

    added custom gym env

--- ppo/stable_baselines_ex.py
+++ ppo/stable_baselines_ex.py
@@ -4,16 +4,21 @@ import gymnasium as gym
 import cv2
 
 from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
+from custom_env.gym_env import ChopperScape
+
+# env = gym.make("CartPole-v1", render_mode="rgb_array")
 # env = gym.make("LunarLander-v2", render_mode="rgb_array")
+env = ChopperScape()
+check_env(env)
 
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=4 * 25000)
+model.learn(total_timesteps=20 * 25000)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(100000):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     cv2.imshow("win", vec_env.render())

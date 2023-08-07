737c7f068b359cf2f827b148892262d64d3dbf51
aoberai <aoberai05@gmail.com>
2023-08-06 23:33:17 -0700

    added test dataset

--- transformers/sentiment/attn_encoder.py
+++ transformers/sentiment/attn_encoder.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import sys
 import copy
+import time
 # np.set_printoptions(threshold=sys.maxsize)
 # torch.set_printoptions(profile="full")
 
@@ -15,6 +16,7 @@ TODO:
 test set
 fixed inference
 cross entropy loss, not mse 
+regularization
 """
 
 
@@ -124,10 +126,12 @@ def load(model_path='attn_encoder.pt'):
 # tokenization and ids
 
 tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
-x_train, y_train = [], []
-if "x.pt" not in os.listdir(".") or "y.pt" not in os.listdir("."):
+x_train, y_train, x_test, y_test = [], [], [], []
+if "x.pt" not in os.listdir(".") or "y.pt" not in os.listdir(".") or "x_test.pt" not in os.listdir(".") or "y_test.pt" not in os.listdir("."):
     sst = load_dataset("sst2")
     x_batch_tmp, y_batch_tmp = [], []
+    print("train dataset")
+    time.sleep(3)
     for i in range(1, len(sst["train"])):
         elem = sst["train"][i]
         sentence = elem["sentence"]
@@ -139,7 +143,7 @@ if "x.pt" not in os.listdir(".") or "y.pt" not in os.listdir("."):
         # y_batch_tmp.append([1 if True in [True if word in sentence.split() else False for word in ["the", "and", "but", "or", "a", "is"]] else 0])
         print(sentence)
         print(y_batch_tmp[-1])
-        decoded = tokenizer.decode(encoded)
+        # decoded = tokenizer.decode(encoded)
 
         """
         print("Sentence:", sentence)
@@ -153,13 +157,45 @@ if "x.pt" not in os.listdir(".") or "y.pt" not in os.listdir("."):
             y_batch_tmp.clear()
         # if i > 1000:
         #     break
+    print("test dataset")
+    time.sleep(3)
+    for i in range(0, len(sst["test"])):
+        elem = sst["test"][i]
+        sentence = elem["sentence"]
+        sentence += "[PAD]" * (context_length - len(tokenizer.encode(sentence)))
+        # tokens = tokenizer.tokenize(sentence)
+        encoded = tokenizer.encode(sentence)
+        x_test.append(encoded)
+        y_test.append([elem["label"]])
+        # y_batch_tmp.append([1 if True in [True if word in sentence.split() else False for word in ["the", "and", "but", "or", "a", "is"]] else 0])
+        print(sentence)
+        print(y_batch_tmp[-1])
+        # decoded = tokenizer.decode(encoded)
+
+        """
+        print("Sentence:", sentence)
+        print("Tokens:", tokens)
+        print("Encoded:", encoded)
+        """
 
     x_train = torch.LongTensor(x_train).to(device)
     y_train = torch.Tensor(y_train).to(device)
+
+
+    x_test = torch.LongTensor(x_test).to(device)
+    y_test = torch.Tensor(y_test).to(device)
+
     torch.save(x_train, "x.pt")
     torch.save(y_train, "y.pt")
+
+    torch.save(x_test, "x_test.pt")
+    torch.save(y_test, "y_test.pt")
 else:
     x_train, y_train = torch.load("x.pt").to(device), torch.load("y.pt").to(device)
+    x_test, y_test = torch.load("x_test.pt").to(device), torch.load("y_test.pt").to(device)
+
+    print(x_train.shape, x_test.shape)
+    print(y_train.shape, y_test.shape)
 
 
 model = Transformer().to(device)
@@ -168,6 +204,7 @@ while True:
     try:
         try:
             for epoch in range(28000):
+                batch_loss_avg = []
                 for batch_idx in range(len(x_train)):
                     x_batch, y_batch = x_train[batch_idx], y_train[batch_idx]
 
@@ -179,14 +216,17 @@ while True:
                     y_batch = y_batch[rand_order]
 
                     polarity = model.forward(x_batch)
-                    print(*[tokenizer.decode(x_batch[i]).replace("[PAD]", "") for i in range(-10, -1)], polarity[-10:-1], y_batch[-10:-1], "\n\n\n\n\n",  sep='\n')
-                    mae_loss = torch.nn.L1Loss()(polarity, y_batch).to(device)
+                    # mae_loss = torch.nn.L1Loss()(polarity, y_batch).to(device)
                     mse_loss = torch.nn.MSELoss()(polarity, y_batch).to(device)
-                    print("EPOCH:", epoch, "MAE:", mae_loss.item(), "MSE:", mse_loss.item())
+                    batch_loss_avg.append(mse_loss.item())
+                    # print("EPOCH:", epoch, "MAE:", mae_loss.item(), "MSE:", mse_loss.item())
                     opt.zero_grad()
                     mse_loss.backward()
                     opt.step()
 
+                polarity_test = model.forward(x_test).to(device)
+                print("\n\n\n\n\n", *[tokenizer.decode(x_test[i]).replace("[PAD]", "") for i in range(-10, -1)], polarity_test[-10:-1], y_test[-10:-1],  sep='\n')
+                print("EPOCH:", epoch, "TRAIN MSE:", sum(batch_loss_avg)/len(batch_loss_avg), "TEST MSE:", torch.nn.MSELoss()(polarity_test, y_test).item(), "\n\n\n\n\n")
                 save(model, opt)
 
         except KeyboardInterrupt as e:
